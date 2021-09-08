044dcaaa4a35852853ba2682c2b37ba7c9d6c9ec
Nati Alex <natialex@Natis-MacBook-Air.local>
2021-09-08 17:46:09 -0400

    nit

--- encryptapi/myencrypt/admin.py
+++ encryptapi/myencrypt/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Data
 
 # Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_displa = ('encryption_key', 'encrypted_value')
+
+admin.site.register(Data, DataAdmin)
--- encryptapi/myencrypt/models.py
+++ encryptapi/myencrypt/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 # Create your models here.
- 
+class Data(models.Model):
+    encryption_key = models.CharField(max_length = 200)
+    encrypted_value = models.CharField(max_length = 500)
